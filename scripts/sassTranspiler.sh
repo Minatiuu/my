06b9ff83565d0cc5ccd5a9f3fc898c81283a0ff1
Minatiuu <var.devmyna@gmail.com>
2022-04-17 19:44:01 -0300

    Formação Inicial do SASS

--- scripts/sassTranspiler.sh
+++ scripts/sassTranspiler.sh
@@ -1,2 +1,2 @@
-sass --style=compressed --watch style/css.scss style/css/css.css > logs/transpilers.out &
-sass --watch style/css.scss style/dev_css/css.css > logs/transpilers.out &
+sass --style=compressed --watch style/css.scss style/css/css.css &
+sass --watch style/css.scss style/dev_css/css.css &
