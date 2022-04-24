a68ea2c96d5ee875f3ea5aa36a345a9b0b79583c
Minatiuu <var.devmyna@gmail.com>
2022-04-24 14:35:00 -0300

    Testes e modelos da Interface com Menu

--- server/server.js
+++ server/server.js
@@ -15,23 +15,3 @@ server.listen(PORT);
 io.on('connection', socket => {
     console.log(`> Socket.io conectou-se na porta ${PORT}`);
 });
-
-var discord = new Discord.Discord(clientId, Discord.CreateFlags.Default);
-
-// Request user's avatar data. Sizes can be powers of 2 between 16 and 2048
-imageManager.Fetch(Discord.ImageHandle.User(53908232506183680, 128), (result, handle) =>
-{
-  {
-    if (result == Discord.Result.Ok)
-    {
-      // If you are working in Unity, you can also use GetTexture()
-      // Which is only exposed for Unity builds
-      // These return raw RGBA.
-      var data = imageManager.GetData(handle);
-    }
-    else
-    {
-      Console.WriteLine("image error {0}", handle.Id);
-    }
-  }
-};
